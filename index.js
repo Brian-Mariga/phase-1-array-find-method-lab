dad39873651c43403f864a650001e513a3b5d609
Brian <brianmariga0@gmail.com>
2024-01-11 17:03:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(records) {
+  for (const element of records) {
+    if (element.result === "W") {
+      return element.year;
+    }
+  }
+  return undefined;
+}
+
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
